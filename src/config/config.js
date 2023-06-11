fdef65c2719b141fe8a1ee6834392f6d2bcc9dfe
Rmesonez <rmesonez@gmail.com>
2023-06-11 10:17:32 -0600

    first commit1

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "postgres",
-    "password": "root",
-    "port": 5432,
-    "database": "shopping_cart2",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "port": process.env.DB_PORT,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
@@ -15,10 +17,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "port": process.env.DB_PORT,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
   }
 }
